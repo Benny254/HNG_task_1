7a0caf368950f221f060766a3a7f4eedb90b76a0
Benard Odhiambo <asshimbenny01@gmail.com>
2025-02-09 15:41:58 +0300

    Update urls.py

--- HNG_task_1/Task_1/urls.py
+++ HNG_task_1/Task_1/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path # type: ignore
+from django.urls import re_path # type: ignore
 from .views import ClassifyNumberView
 
 urlpatterns = [
-            path('api/classify-number', ClassifyNumberView.as_view(), name='classify_number'),
+            re_path(r'^api/classify-number/?$', ClassifyNumberView.as_view(), name='classify_number'),
 
             ]
